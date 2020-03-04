7bc8de32e3937d61b365ffb90492ce97c674116c
Giteon <fox.gideon@gmail.com>
2020-03-04 02:25:23 -0800

    php contact form V2

--- contact.php
+++ contact.php
@@ -3,18 +3,18 @@
 	$email =  $_POST['email'];
 	$message =  $_POST['message'];
 
-	$email_from = 'Kahzum Customer';
+	$email_from = 'jugglerfox@gmail.com';
 	$email_subject = 'New Message From Kahzum website';
 	$email_body = "Name: $name.\n".
 				  "Email: $email.\n".
 				  "Message: $message".\n";
 
-	$to ="jugglerfox@gmail.com";
-	$headers = "From: $email_from \r\n";
-	$headers .= "Reply-To: $email \r\n";
+	$to = "jugglerfox@gmail.com";
+	$headers = "From: $email_from r\n";
+	$headers .= "Reply-To: $email \n";
 
 	mail($to,$email_subject,$email_body,$headers);
 
-	header("location: index.html");
+	header("Location: index.html");
 
 ?>
